6132c921585600cf1c9511eb7b9753b8ed232941
Sanyam <sanyam15111@gmail.com>
2020-05-15 11:34:18 +0530

    Connected Webpages

--- my_app/views.py
+++ my_app/views.py
@@ -5,4 +5,8 @@ def home(request):
     return render(request,template_name='base.html')
 
 def new_search(request):
-    return render(request,template_name='my_app/new_search.html')
+    search=request.POST.get('searchy')
+    print(search)
+    stuff_for_frontend={'search':search
+                        }
+    return render(request,template_name='my_app/new_search.html',context=stuff_for_frontend)
